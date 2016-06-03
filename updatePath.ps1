f4a72ef2ee528565a0467b177ee281a06c51ff85
Chris Ballance <ballance@gmail.com>
2016-06-03 10:07:44 -0400

    Improvements to updatePath to accept command line params

--- updatePath.ps1
+++ updatePath.ps1
@@ -1,11 +1,17 @@
-### Modify system environment variable ###
-[Environment]::SetEnvironmentVariable
-     ( "Path", $env:Path, [System.EnvironmentVariableTarget]::Machine )
+function updatePath {
+param (
+	[string] $pathToAdd
+)
+	### Modify system environment variable ###
+	[Environment]::SetEnvironmentVariable
+	     ( "Path", $env:Path, [System.EnvironmentVariableTarget]::Machine )
+	
+	### Modify user environment variable ###
+	[Environment]::SetEnvironmentVariable
+	     ( "INCLUDE", $env:INCLUDE, [System.EnvironmentVariableTarget]::User )
+	
+	### Usage from comments - Add to the system environment variable ###
+	[Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + $pathToAdd, [EnvironmentVariableTarget]::Machine)
+}
 
-### Modify user environment variable ###
-[Environment]::SetEnvironmentVariable
-     ( "INCLUDE", $env:INCLUDE, [System.EnvironmentVariableTarget]::User )
-
-### from comments ###
-### Usage from comments - Add to the system environment variable ###
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";L:\home\tools\phantomjs-2.1.1-windows\bin", [EnvironmentVariableTarget]::Machine)
+updatePath $input
